2a0ccc6a9882c7f45725a7b8f9bb9c5ed75673d1
Robbo <robbo.clancy@gmail.com>
2016-11-17 06:22:14 +1000

    first workspace now opening correctly

--- .i3/workspaces.sh
+++ .i3/workspaces.sh
@@ -8,32 +8,36 @@ do
     i3-msg "workspace $i; append_layout $HOME/.i3/workspace-$i.json"
     if [ $i = 1 ]
     then
-      i3-msg exec google-chrome-beta
+      i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects"'
+      i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects"'
+      i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects"'
+      i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects"'
+      i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects"'
     fi
-    if [ $i = 2 ]
-    then
-      i3-msg exec vivaldi-snapshot
-    fi
-    if [ $i = 3 ]
-    then
-      i3-msg exec ~/Komodo-IDE-10/bin/komodo
-      i3-msg exec dbus-launch gnome-terminal
-      i3-msg exec dbus-launch gnome-terminal
-      i3-msg exec dbus-launch gnome-terminal
-      i3-msg exec dbus-launch gnome-terminal
-    fi
-    if [ $i = 6 ]
-    then
-      i3-msg exec dbus-launch gnome-terminal
-      i3-msg exec dbus-launch gnome-terminal
-      i3-msg exec dbus-launch gnome-terminal
-      i3-msg exec dbus-launch gnome-terminal
-    fi
-    if [ $i = 8 ]
-    then
-      i3-msg exec spotify
-      i3-msg exec google-chrome-stable
-    fi
+    # if [ $i = 2 ]
+    # then
+    #   i3-msg exec vivaldi-snapshot
+    # fi
+    # if [ $i = 3 ]
+    # then
+    #   i3-msg exec ~/Komodo-IDE-10/bin/komodo
+    #   i3-msg exec dbus-launch gnome-terminal
+    #   i3-msg exec dbus-launch gnome-terminal
+    #   i3-msg exec dbus-launch gnome-terminal
+    #   i3-msg exec dbus-launch gnome-terminal
+    # fi
+    # if [ $i = 6 ]
+    # then
+    #   i3-msg exec dbus-launch gnome-terminal
+    #   i3-msg exec dbus-launch gnome-terminal
+    #   i3-msg exec dbus-launch gnome-terminal
+    #   i3-msg exec dbus-launch gnome-terminal
+    # fi
+    # if [ $i = 8 ]
+    # then
+    #   i3-msg exec spotify
+    #   i3-msg exec google-chrome-stable
+    # fi
     printf "done\n"
   else
     printf "skipping, no file found\n"
