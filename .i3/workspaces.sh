23057997ead7d0310f5264070a4b315dd6220358
Robbo <robbo.clancy@gmail.com>
2016-11-24 23:29:23 +1000

    fixes and dunst stuff

--- .i3/workspaces.sh
+++ .i3/workspaces.sh
@@ -24,7 +24,7 @@ do
     if [ $i = 3 ]
     then
       i3-msg "move workspace to output DP2-3; append_layout $HOME/.i3/workspace-$i.json"
-      i3-msg exec dbus-launch atom /home/robbo/projects/marketplace && sleep 0.2
+      i3-msg exec 'dbus-launch atom /home/robbo/projects/postedin/marketplace' && sleep 2
       i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects/postedin/marketplace"' && sleep 0.2
       i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects/postedin/marketplace/api"' && sleep 0.2
       i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects/postedin/marketplace"'
@@ -32,7 +32,7 @@ do
     if [ $i = 4 ]
     then
       i3-msg "move workspace to output DP2-3; append_layout $HOME/.i3/workspace-$i.json"
-      i3-msg exec dbus-launch atom /home/robbo/projects/beta && sleep 0.2
+      i3-msg exec 'dbus-launch atom /home/robbo/projects/postedin/beta' && sleep 2
       i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects/postedin/beta"' && sleep 0.2
       i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects/postedin/beta/public/app/public"' && sleep 0.2
       i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects/postedin/beta/public/app/admin"'
@@ -40,7 +40,7 @@ do
     if [ $i = 5 ]
     then
       i3-msg "move workspace to output DP2-3; append_layout $HOME/.i3/workspace-$i.json"
-      i3-msg exec dbus-launch atom /home/robbo/projects/editors && sleep 0.2
+      i3-msg exec 'dbus-launch atom /home/robbo/projects/postedin/editors' && sleep 2
       i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects/postedin/editors"' && sleep 0.2
       i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects/postedin/editors"' && sleep 0.2
       i3-msg exec 'dbus-launch gnome-terminal --working-directory="/home/robbo/projects/postedin/editors"'
@@ -58,8 +58,8 @@ do
     if [ $i = 8 ]
     then
       i3-msg "move workspace to output DP2-2; append_layout $HOME/.i3/workspace-$i.json"
-      i3-msg exec dbus-launch discord && sleep 0.2
-      i3-msg exec dbus-launch spotify && sleep 0.2
+      #i3-msg exec dbus-launch discord-canary && sleep 0.2
+      #i3-msg exec dbus-launch spotify && sleep 0.2
     fi
     printf "done\n"
   else
